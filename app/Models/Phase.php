08da412622943489dd6872f5389d2ef5697ab403
baishakhipatra <baishakhi.patra@techmantra.co>
2025-11-19 19:05:12 +0530

    changes on candidates and phase 19-11-25

--- app/Models/Phase.php
+++ app/Models/Phase.php
@@ -9,18 +9,19 @@ class Phase extends Model
    protected $table = 'phases';
 
    protected $fillable = [
-        'name', 'last_date_of_nomination', 'date_of_election' 
+        'name', 'last_date_of_nomination', 'date_of_election','last_date_of_mcc'
    ];
 
-  // ✅ Relationship to PhaseWiseAssembly
+  // Relationship to PhaseWiseAssembly
     public function phaseAssemblies()
     {
         return $this->hasMany(PhaseWiseAssembly::class, 'phase_id', 'id');
     }
 
-    // ✅ Direct relationship to Assembly through the pivot
+    //  Direct relationship to Assembly through the pivot
     public function assemblies()
     {
         return $this->belongsToMany(Assembly::class, 'phase_wise_assembly', 'phase_id', 'assembly_id');
     }
+
 }
