7eea2fc5775f52505165d7cf9182906a29170d84
PujaTechMantra <puja@techmantra.co>
2026-01-07 20:05:59 +0530

    form 26

--- app/Models/NominationForm26.php
+++ app/Models/NominationForm26.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class NominationForm26 extends Model
 {
+    protected $table = 'form_26';
+    
+    protected $fillable = [
+    'relation_type',
+    'relation_name',
+    'address',
+    'assembly_constituency_no',
+    'assembly_constituency_serial_no',
+    'assembly_constituency_part_no',
+    'phone_no',
+    'alternative_phone_no',
+    'candidate_epic_no',
+    'photograph',
+    'caste_tribe_details',
+    'proposer_epic',
+    'email_id',
+    'social_media_accounts',
+    'pan_and_itr_details',
+    'last_5_yrs_income',
+    'movable_and_immovable_assets',
+    'loans_govt_dues',
+    'occupation',
+    'sources_of_income',
+    'highest_educational_qualification',
+];
     
 }
