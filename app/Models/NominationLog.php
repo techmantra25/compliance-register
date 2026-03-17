ef35385df1ad41c71afdb1feffd6e9f9fa8b75e3
Puja <puja@techmantra.co>
2026-03-17 19:55:22 +0530

    Merge

--- app/Models/NominationLog.php
+++ app/Models/NominationLog.php
@@ -9,7 +9,6 @@ class NominationLog extends Model
     protected $fillable = [
         'nomination_id',
         'form_data',
-        'form_json',
         'pdf_file',
         'generated_by',
         'ip_address'
